7f6b1c25a0c5c9739f8e06454157eb46b63a94d2
Collin Smith <cmsmith.tx@gmail.com>
2019-09-28 14:48:10 -0500

    cleaning up code

--- String_Problems/spinalCase.js
+++ String_Problems/spinalCase.js
@@ -1,11 +1,8 @@
 // Spinal Case a given string
 // write a function that converts any string to spinal case (all lowercase words joined by dashes)
-// e.g. spinalCase('The WEATHER isReallyGreat') = the-weather-is-really-great
+// e.g. spinalCase('AlwaysSunny_inPhiladelphia') = always-sunny-in-philadelphia
 
 const spinalCase = str => {
-
     let newStr = str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase();
     return newStr;
 }
-    
-    //console.log(spinalCase('AlwaysSunny_inPhiladelphia'));
