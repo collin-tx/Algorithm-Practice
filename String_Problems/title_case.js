0cd335654ab2a2a0034c150cd6845269514cee2b
Collin Smith <cmsmith.tx@gmail.com>
2019-09-28 14:30:19 -0500

    refactoring solution

--- String_Problems/title_case.js
+++ String_Problems/title_case.js
@@ -2,13 +2,13 @@
 // given a string with any combo of capitalizations, return a string that is title cased
 // e.g. titleCase('onE SMALL steP FOr MaN') = 'One Small Step For Man'
 
-const titleCase = (str) => {
+const titleCase = str => {
     let newStr = str.toLowerCase().split(' ');
-    let finalStr = [];
+    let final = [];
     
     for (let word of newStr){
-        word = word[0].toUpperCase() + word.slice(1, word.length);
-        finalStr.push(word);
+        word = word[0].toUpperCase() + word.slice(1);
+        final.push(word);
     }
-    return finalStr.join(' ');
+    return final.join(' ');
 }
