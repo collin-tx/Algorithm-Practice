15b1282cb37f4d35a27df4ae8c2e03b2beb40476
Collin Smith <cmsmith.tx@gmail.com>
2019-09-28 14:55:43 -0500

    added alternative solution

--- Number_Problems/factorialize.js
+++ Number_Problems/factorialize.js
@@ -7,3 +7,16 @@
 const factorialize = (num) => {
   return num === 0 ? 1 : num * factorialize(num - 1);
 }
+
+
+//or solve using loop + reduce
+
+const factorializeLoop = num => {
+  let nums = [];
+  if (num === 1 || num === 0) return num;
+  if (num < 0) return 'try a positive number!'
+  for (let i=num; i>1; i--){
+    nums.push(i);
+  }
+  return nums.reduce((a,b) => a * b);
+}
