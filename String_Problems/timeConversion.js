b73d4bcd112e30c3eb36bb3964f9df34303cf458
Collin Smith <cmsmith.tx@gmail.com>
2019-09-30 13:56:50 -0500

    cleaning up code

--- String_Problems/timeConversion.js
+++ String_Problems/timeConversion.js
@@ -4,11 +4,9 @@
 
 const toMilitaryTime = time => {
     let hour = Number(time.slice(0, 2));
-
     if(time.includes('AM')){
         return (hour === 12 ? '00' + time.slice(2, time.length - 2) : time.slice(0, time.length - 2));
     }
-
     let pmHour = hour === 12 ? 12 : hour + 12;
     let converted = (String(pmHour)+ time.slice(2, time.length - 2));
     return converted;
