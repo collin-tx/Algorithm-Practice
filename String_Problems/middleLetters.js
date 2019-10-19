96ab171c4cad6dc5da10ea475b9dec25dd2ad7df
Collin Smith <cmsmith.tx@gmail.com>
2019-10-19 13:09:34 -0500

    adding solution

---
+++ String_Problems/middleLetters.js
@@ -0,0 +1,13 @@
+//Create a function that takes a string and returns the middle character(s). 
+// If the word's length is odd, return the middle character.
+// If the word's length is even, return the middle two characters.
+
+function getMiddle(str) {
+	if (str.length % 2 !== 0){
+		return str.charAt(((str.length - 1)/2));
+	}
+	return str.slice((str.length / 2) - 1, (str.length / 2) + 1);
+}
+
+console.log(getMiddle('testing'));
+console.log(getMiddle('collin'));
