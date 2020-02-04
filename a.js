d8d7cfdd109139c38bd91fc9b610a5307fbcbe7b
Collin Smith <cmsmith.tx@gmail.com>
2020-02-04 16:19:27 -0600

    removing unnecessary file

--- a.js
+++
@@ -1,9 +0,0 @@
-// Write a function that takes two or more arrays and returns a new 
-// array of unique values in the order of the original provided arrays.
-
-const unionSort = (...args) => {
-
-  let flattenedArr = [].concat(...args);
-
-  return Array.from(new Set(flattenedArr));
-}
